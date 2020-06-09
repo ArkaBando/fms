e0f66cd4c01d76e8007990805f970d57262cc101
arkabandyopadhyay <arkabandyopadhyay@outlook.com>
2020-06-10 04:39:39 +0530

    Updates

---
+++ precache-manifest.97f16c71fa07a525a253754f0f3e0616.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "debcee894c55db30cb8fa173012685c1",
+    "url": "/fms/index.html"
+  },
+  {
+    "revision": "cd54c44ea1f8d566598e",
+    "url": "/fms/static/css/main.4e3af656.chunk.css"
+  },
+  {
+    "revision": "9b939c9226c93ff14dca",
+    "url": "/fms/static/js/2.3a823c7e.chunk.js"
+  },
+  {
+    "revision": "fbfb5e42a41e7004782949e93fdf296a",
+    "url": "/fms/static/js/2.3a823c7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd54c44ea1f8d566598e",
+    "url": "/fms/static/js/main.ded7e3d0.chunk.js"
+  },
+  {
+    "revision": "98d4728a8e3c6de3ddc1",
+    "url": "/fms/static/js/runtime-main.62d744b0.js"
+  },
+  {
+    "revision": "e8390dadf9d1a082c6334496fbe5e016",
+    "url": "/fms/static/media/addfolder.e8390dad.jpg"
+  }
+]);
